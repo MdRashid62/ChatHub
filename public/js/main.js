abffe0d80f04eafdacf08320044764e949fba5cd
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-05-05 15:34:41 +0500

    commit changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+
 const socket = io();
+
+// Join chat room
+socket.emit('joinRoom', { username, room });
 
 // Message from server
 socket.on('message', message => {
@@ -32,9 +40,9 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">Rashid <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`;
     document.querySelector('.chat-messages').append(div);
 }
--- server.js
+++ server.js
@@ -1,33 +1,48 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const { Socket } = require('dgram');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "ChatHub Bot";
 
 // run when client connects
-io.on('connection', socket => {
-    // Welcome current user
-    socket.emit('message', 'Wellcome to CHatHub!');
+io.on("connection", (socket) => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
 
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat!');
+        socket.join(user.room);
+
+    // Welcome current user
+  socket.emit("message", formatMessage(botName, "Wellcome to CHatHub!"));
+
+  // Broadcast when a user connects
+  socket.broadcast.to(user.room).emit(
+    "message",
+    formatMessage(botName, `${user.username} has joined the chat!`)
+  );
+    });
+
+  // Listen for chat message
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
 
     // Runs when client disconnect
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat!');
-    });
+    socket.on("disconnect", () => {
+        io.emit("message", formatMessage(botName, "A user has left the chat!"));
+      });
 
-    // Listen for chat message
-    socket.on('chatMessage', (msg) =>{
-        io.emit('message', msg);
-    });
 });
 
 const PORT = 3000 || process.env.PORT;
